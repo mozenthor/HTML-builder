29ed3bec147acc0bc6cba8e0068941800a1786b3
Aleksandr Karapetov <karapet.yo@gmail.com>
2022-05-12 05:42:32 +0300

    refactor: 03 files in folder feat: 04 copy directory

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,42 +1,27 @@
 const fs = require('fs');
 const path = require('path');
 
-const poi = (way) => {
+const f = (way) => {
   fs.readdir(way,{withFileTypes: true}, (error, files) => {
     if (error) {
       throw error;
     } else {
       files.forEach(file => {
         if (file.isFile()) {
-          if (path.extname(`${way}/${file.name}`) === '') {
-            const name = file.name;
-            const extname = 'no have';
-            fs.stat(`${way}/${file.name}`, (error, stat) => {
-              if (error) {
-                throw error;
-              } else {
-                const size = stat.size/1000;
-                console.log(`${name} - ${extname} - ${size}kb`);
-              }
-            });
-          } else {
-            const extname = path.extname(`${way}/${file.name}`);
-            const name = file.name.split('.')[0];
-            fs.stat(`${way}/${file.name}`, (error, stat) => {
-              if (error) {
-                throw error;
-              } else {
-                const size = stat.size/1000;
-                console.log(`${name} - ${extname.slice(extname.indexOf('.')+1)} - ${size}kb`);
-              }
-            });
-          }
-        } else {
-          poi(`${way}/${file.name}`);
+          const extname = path.extname(`${way}/${file.name}`);
+          const name = file.name;
+          fs.stat(`${way}/${file.name}`, (error, stat) => {
+            if (error) {
+              throw error;
+            } else {
+              const size = stat.size/1000;
+              console.log(`${name.slice(0, name.lastIndexOf('.'))} - ${extname.slice(extname.indexOf('.')+1)} - ${size}kb`);
+            }
+          });
         }
       });
     }
   });
 };
 
-poi('./03-files-in-folder/secret-folder');
+f('./03-files-in-folder/secret-folder');
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs = require('fs');
 
+fs.mkdir('./04-copy-directory/files-copy',{recursive: true}, err => {
+  if (err) throw err;
+});
+
+
+fs.readdir('./04-copy-directory/files', (err,files) => {
+  if (err) {
+    throw err;
+  } else {
+    fs.readdir('./04-copy-directory/files-copy', (err,files1) => {
+      if (err){
+        throw err;
+      }else {
+        files1.forEach (file1 => {
+          fs.unlink(`./04-copy-directory/files-copy/${file1}`, err => {
+            if (err) throw err;
+          });
+        });
+        files.forEach (file => {
+          ReadWrite(file);
+        });
+      }
+    });
+  }
+});
+
+const ReadWrite = (file) => {
+  const ReadStream  = fs.createReadStream(`./04-copy-directory/files/${file}`);
+  const WriteStream = fs.createWriteStream(`./04-copy-directory/files-copy/${file}`);
+  ReadStream.on('data', function (chunk) {
+    WriteStream.write(`${chunk.toString()}`);
+  });
+};
